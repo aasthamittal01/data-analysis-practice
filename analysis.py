1d298a7921f2e00f6864c0825cbcef5519ce0c27
aastha mittal <aasthamittal64@gmail.com>
2026-02-22 20:29:07 +0800

    updated analysis file

--- analysis.py
+++ analysis.py
@@ -1,1 +1,16 @@
-print("my data analysis journey begins")
+import pandas as pd
+
+print("Data Analysis Practice Started")
+
+data = {
+    "Student": ["A", "B", "C", "D"],
+    "Marks": [85, 90, 78, 92]
+}
+
+df = pd.DataFrame(data)
+
+print("\nDataset:")
+print(df)
+
+print("\nAverage Marks:")
+print(df["Marks"].mean())
